6e67275168c497f8d393927453f68812371532d8
Tew0979271981 <zxrty721@gmail.com>
2025-05-24 12:08:17 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,29 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const uri = "mongodb+srv://zxrty721:KD5fljPJPg7TOgq0@cluster0.gwizz1y.mongodb.net/";
+const uri = "mongodb+srv://zxrty721:KD5fljPJPg7TOgq0@cluster0.gwizz1y.mongodb.net/database?retryWrites=true&w=majority";
 
 mongoose.connect(uri)
   .then(() => console.log("✅ Connected to MongoDB"))
-  .catch(err => console.error("❌ MongoDB error:", err.message));
+  .catch(err => console.error("❌ MongoDB connection error:", err.message));
 
+  
 app.get("/", (req, res) => {
   res.send("Web API is running!");
 });
 
-app.get("/users", async (req, res) => {
-  console.log("✅ /users route called");
-
+app.get("/items", async (req, res) => {
   try {
     const db = mongoose.connection.db;
-    const users = await db.collection("users").find({}).toArray();
-    console.log("✅ Users:", users);
-    res.json(users);
+    console.log(db.databaseName)
+    const items = await db.collection("items").find({}).toArray();
+
+    console.log("📦 Items:", items); 
+    res.json(items);
   } catch (err) {
-    console.error("❌ Error:", err.message);
+    console.error("❌ /items error:", err.message);
     res.status(500).json({ error: err.message });
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`✅ Server listening on port ${port}`));
+const port = 3000;
+app.listen(port, () => console.log(`✅ Server running at http://localhost:${port}`));
